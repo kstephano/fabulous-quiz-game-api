23be037595f05024314c0325f1752089ddd7404d
Kelvin <kstephano@gmail.com>
2022-02-18 16:25:38 +0000

    removed redudant variable

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
-const express = require('express');
-const cors = require('cors');
-const userRoutes = require('./controllers/users')
-const lobbyRoutes = require('./controllers/lobbies')
+const express = require("express");
+const cors = require("cors");
+const userRoutes = require("./controllers/users");
+const lobbyRoutes = require("./controllers/lobbies");
 
 const server = express();
 server.use(cors());
 server.use(express.json());
-server.use('/users', userRoutes)
-server.use('/lobbies', lobbyRoutes)
-
-const port = process.env.PORT || 3000;
+server.use("/users", userRoutes);
+server.use("/lobbies", lobbyRoutes);
 
 // Root route
-server.get('/', (req, res) => res.send('Hello, world!'))
+server.get("/", (req, res) => res.send("Hello, world!"));
 
-module.exports = server
+module.exports = server;
